53eb8e38372f7c99a41425b6b44ef73ca0199ebb
Laura Mutu <laura.mutu@carleton.ca>
2014-08-14 03:14:59 -0400

    Initial Version
    Adding base files.
    UART Driver pack

--- RTE/RTE_Components.h
+++ RTE/RTE_Components.h
@@ -10,5 +10,20 @@
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
 
+#include <stdint.h>
+		#include <stdbool.h>
+		#include "inc/hw_ints.h"
+		#include "inc/hw_gpio.h"
+		#include "inc/hw_types.h"
+		#include "driverlib/debug.h"
+		#include "driverlib/fpu.h"
+		#include "driverlib/gpio.h"
+		#include "driverlib/interrupt.h"
+		#include "driverlib/pin_map.h"
+		#include "driverlib/rom.h"
+		#include "driverlib/sysctl.h"
+		#include "driverlib/uart.h"
+		#include "driverlib/rom_map.h"
+		#define CMSIS_Drivers_USART0              /* Driver USART0 */
 
 #endif /* RTE_COMPONENTS_H */
